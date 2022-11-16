f20a68120ed0d3c2edc2524eb3ec6091d5af7d10
Bob <bob@bianisoft.com>
2022-11-16 14:40:24 +0200

    Included some comments

--- src/main.py
+++ src/main.py
@@ -3,47 +3,61 @@ import json
 
 
 def addModificationToTree(treeUp, lstData):
-    treeUp[0] += 1
-    data = lstData.pop(0)
+    """
+        Add a directory string list group into the tree structure
+    """
 
+    # Whenever we add a file into a set of directory, increase the count of all directories
+    treeUp[0] += 1
+
+    # getting this level from the directory group, if not exist initiate it
+    data = lstData.pop(0)
     if data not in treeUp[1]:
         treeUp[1][data] = [0, {}]
 
+    # Recursively add the popped group of directories
     if len(lstData) != 0:
         addModificationToTree(treeUp[1][data], lstData)
 
 
 def main():
+    """
+        Main function of the script
+    """
+
     # Read the CSV File
-    with open(f"./data/query_1(2).csv", mode="r", newline="\n") as fileInput:
+    with open(f"./data/modifiedFiles.csv", mode="r", newline="\n") as fileInput:
         objReader = csv.reader(fileInput)
 
-        next(objReader)
-        # For each row, get row[1]
+        next(objReader)     # Skip header wor
         cpt = 0
         treeModifications = [0, {}]
         while True:
             try:
                 row = next(objReader)
             except StopIteration as e1:
+                # Normal End, quit the loop
                 break
             except BaseException as e:
+                # Rare error where there is a NUL character in the csv
                 print(e)
                 if e.args[0] == "line contains NUL":
                     continue
 
+            # Initiate the tree with the hostname
             hostname = row[7]
             addModificationToTree(treeModifications, [hostname])
 
+            # Then column 0 contains the full path where we organize in tree
             groupFolder = row[0].split("\\")
             addModificationToTree(treeModifications[1][hostname], groupFolder)
 
+            # Debug information to show that things are still running
             cpt += 1
-
             if cpt % 1000 == 0:
                 print(f"Processed {cpt}")
 
-        # print(treeModifications)
+        # Output a dump of the Dictionary as JSON
         with open(f"./data/output.json", mode="w", newline="\n") as fileOutput:
             json.dump({"data": treeModifications}, fileOutput)
 
